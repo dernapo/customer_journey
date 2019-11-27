24dde71137ec50d6c86590f1ac9d9f2bc23a1bb9
dernapo <javier-github@saenz.info>
2019-11-27 22:16:29 +0100

    Cleaning up

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # customer_journey
+
+We want to analize the customer journey, where the cutomer has just to options A or B that repeates in a loop.
+
+Some examples of customer jouneys: 
+   - A
+   - AB
+   - BA
+   - BBB
+   - BAB
--- test_dt_string.R
+++ test_dt_string.R
@@ -1,14 +1,44 @@
+#########################################################
+## Evaluate frequency of customer journeys
+#########################################################
+
+## Load libraries ####
 library(data.table)
+library(ggplot2)
+library(dplyr)
+library(scales)
 
+## Test data ####
 
-dt <- data.table(a=letters[sample(1:2, 200, replace=T)],
-                 b=LETTERS[1:5],
+dt <- data.table(options = LETTERS[sample(1:2, 200, replace=T)],
+                 b=letters[1:5],
                  ID=sample(1:300, 200, replace=T))
 
-dt_summary <- dt[order(b)][, .(ad = paste0(a, collapse = ""), count = .N), by = ID]
+
+## Evaluation ####
+
+dt_summary <- dt[order(b)][, .(journey = paste0(options, collapse = ""), count = .N), by = ID]
 
 
-dt_summary2 <- dt_summary[, .(count_n = .N), .(ad, count)][, quota := count_n/sum(count_n), .(count)]
+dt_summary2 <- dt_summary[, .(count_n = .N), .(journey, count)][, quota := count_n/sum(count_n), .(count)]
 
+
+## Display result ####
 
 dt_summary2[count == 2, -c("count_n", "count")][order(-quota)]
+
+
+## Visualization ####
+
+dt_summary2[count == 2] %>% 
+  ggplot(aes(x=journey, y = quota, fill = journey, label = paste0(100 *round(quota, 2), "%"))) +
+  geom_col() +
+  geom_text(hjust = 0) +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  scale_fill_viridis_d() +
+  theme(legend.position = "none") + 
+  coord_flip()
+
+
+
+
